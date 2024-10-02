ea018e52e32c886716f1501c3fd5c6c4d67076d3
Daniel Torres <62557742+Daniel-Torres2000@users.noreply.github.com>
2024-10-02 14:27:47 -0600

    Add files via upload

---
+++ Material de aprendizaje/C#/016 - Parametros y Clases/AplicacionParametros/AplicacionParametros/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionParametros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10, 20);
+            double area = rect.ObtenArea();
+            Console.WriteLine("Resultado del area: " + area);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Material de aprendizaje/C#/016 - Parametros y Clases/AplicacionParametros/AplicacionParametros/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionParametros
+{
+    class Rectangle
+    {
+        private double length, width;
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        public double ObtenArea()
+        {
+            return length * width;
+        }
+    }
+}
