012a29036ff6979a5d13cdfa5ebfb51d440e2264
Daniel Torres <62557742+Daniel-Torres2000@users.noreply.github.com>
2024-10-02 16:24:10 -0600

    Add files via upload

---
+++ Material de aprendizaje/C#/21 - Operaciones Contables/Monto de Empresa/Monto de Empresa/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monto_de_Empresa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2.	UNA EMPRESA QUE SE DEDICA AL CULTIVO DE PRODUCTOS AGRICOLAS, 
+                    DESEA DETERMINAR EL MONTO TOTAL DE LA INVERSION QUE SE DEBE REALIZAR, 
+                    ESTE DEPENDE DE CIERTOS ASPECTOS COMO LO SON:
+                      a.	ALQUILER DE TERRENO 1 CUERDA CUESTA 1000
+                      b.	SEMILLAS: 150 EL QUINTAL POR CADA CUERDA
+                      c.	SEGURO: 1000 POR CADA CUERDA
+                      d.	GASTOS DE SIEMBRA Y CUIDADO: 1500 POR CADA CUERDA
+                    SI LA PERSONA DESEA SEMBRAR DETERMINADA CANTIDAD DE CUERDAS. ¿A CUANTO ASCIENDE EL MONTO DE LA INVERSION?
+            */
+
+            Int16 cuerda;
+            Double alquiler, semillas, seguro, gastos, total;
+
+            Console.WriteLine("INGRESE LA CANTIDAD DE CUERDAS");
+            cuerda = Convert.ToInt16(Console.ReadLine());
+            alquiler = cuerda * 1000;
+            semillas = cuerda * 150;
+            seguro = cuerda * 1000;
+            gastos = cuerda * 1500;
+            total = alquiler + semillas + seguro + gastos;
+
+            Console.WriteLine("ALQUILER: Q" + alquiler);
+            Console.WriteLine("SEMILLAS: Q" + semillas);
+            Console.WriteLine("SEGURO: Q" + seguro);
+            Console.WriteLine("GASTOS DE SIEMBRA Y CUIDADO: Q" + gastos);
+            Console.WriteLine("EL TOTAL DEL MONTO ES: Q{0}", total);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Material de aprendizaje/C#/21 - Operaciones Contables/Monto de Empresa/Monto de Empresa/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Monto de Empresa")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Monto de Empresa")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("33e66155-74f9-42d4-839c-58a9966702db")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
