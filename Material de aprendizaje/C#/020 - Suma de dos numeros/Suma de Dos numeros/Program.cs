6193e9f1e392663831e8f287768c002781e19000
Daniel Torres <62557742+Daniel-Torres2000@users.noreply.github.com>
2024-10-02 16:20:57 -0600

    Add files via upload

---
+++ Material de aprendizaje/C#/020 - Suma de dos numeros/Suma de Dos numeros/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma_de_Dos_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, suma;
+
+            Console.WriteLine("INGRESE UN NUMERO: ");
+            num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("INGRESE UN NUMERO: ");
+            num2 = Convert.ToInt16(Console.ReadLine());
+
+            suma = num1 + num2;
+
+            Console.WriteLine("EL RESULTADO DE LA SUMA ES: {0}", suma);
+
+            Console.ReadKey();
+
+        }
+    }
+}
