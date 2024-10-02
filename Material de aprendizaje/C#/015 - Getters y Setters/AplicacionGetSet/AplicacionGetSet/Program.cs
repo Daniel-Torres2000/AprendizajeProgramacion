62535b3dad5678c32c7a7c8f142b69d4f8be30d5
Daniel Torres <62557742+Daniel-Torres2000@users.noreply.github.com>
2024-10-02 12:35:57 -0600

    Add files via upload

---
+++ Material de aprendizaje/C#/015 - Getters y Setters/AplicacionGetSet/AplicacionGetSet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionGetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Length = 10;
+            rect.Width = 20;
+            double area = rect.ObtenerArea();
+            Console.WriteLine("El resultado del area es: " + area);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Material de aprendizaje/C#/015 - Getters y Setters/AplicacionGetSet/AplicacionGetSet/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionGetSet
+{
+    class Rectangle
+    {
+        private double length, width;
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if(value > 0.0)
+                {
+                    length = value;
+                }
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value > 0.0)
+                {
+                    width = value;
+                }
+            }
+        }
+        public double ObtenerArea()
+        {
+            return width * length;
+        }
+    }
+}
