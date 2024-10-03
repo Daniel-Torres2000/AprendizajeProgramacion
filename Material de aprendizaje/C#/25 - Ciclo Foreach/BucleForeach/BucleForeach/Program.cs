8b33c08b7f0c57b3f601ebd6df4eadc6f36c628b
Daniel Torres <62557742+Daniel-Torres2000@users.noreply.github.com>
2024-10-03 15:55:52 -0600

    Add files via upload

---
+++ Material de aprendizaje/C#/25 - Ciclo Foreach/BucleForeach/BucleForeach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BucleForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vectorr = { 1, 2, 3, 4, 5 };
+            foreach(int i in vectorr)
+            {
+                Console.WriteLine("Posición i = {0}", i);
+                Console.ReadKey();
+            }
+        }
+    }
+}
