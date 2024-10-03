aacf04e7e25cf0eed1d7e9ecb149fe676072a7b9
Daniel Torres <62557742+Daniel-Torres2000@users.noreply.github.com>
2024-10-03 15:58:37 -0600

    Add files via upload

---
+++ Material de aprendizaje/C#/27 - Condiciones Iniciar Sesion/Ejemplo 1/Ejemplo1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Realizar un programa que simule iniciar sesion en office 365, pida un correo electronico y una contraseña,
+              se debe verificar que ambos sean correctos, de lo contario debe mostrar, mensaje datos invalidos.
+              Los datos base son:
+              Correo = "lostomatitos123@icvh.edu.gt, Contraseña = "LaS0pa"*/
+            string correo, contra;
+            Console.WriteLine("INGRESE CORREO ELECTRONICO: ");
+            correo = Console.ReadLine();
+            //Debemos recordar que los datos tipo string no se deben convertir
+            Console.WriteLine("INGRESE CONTRASEÑA: ");
+            contra = Console.ReadLine();
+
+            if((correo=="lostomatitos123@icvh.edu.gt")&&(contra=="LaS0pa"))
+            {
+                Console.WriteLine("BIENVENIDO");
+            }
+            else
+            {
+                Console.WriteLine("DATOS INVALIDOS");
+            }
+            Console.ReadKey();
+        }
+    }
+}
