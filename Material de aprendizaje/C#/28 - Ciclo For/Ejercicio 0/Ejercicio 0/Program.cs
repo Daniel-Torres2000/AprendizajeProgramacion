e042c69683bcdbcecb809091c1d1d4812facafa3
Daniel Torres <62557742+Daniel-Torres2000@users.noreply.github.com>
2024-10-03 16:02:28 -0600

    Add files via upload

---
+++ Material de aprendizaje/C#/28 - Ciclo For/Ejercicio 0/Ejercicio 0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Realizar un programa que haga un ciclo y muestre 7 veces el texto hola mundo*/
+            for(int i = 1; i <= 7; i++)
+            {
+                Console.WriteLine("HOLA MUNDO");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Material de aprendizaje/C#/29 - Condiciones Si Y/Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Condicion Si Y, realizar un programa que pida una edad, y muestre el respectivo rango al que pertenece:
+             
+               niño = 1-10 años
+               adeolescente = 11-17 años
+               adulto = 18-40 años
+               mayor = 41-en adelante*/
+
+            int edad;
+
+            //La diferencia de no colocar line seguido de write, es que no nos brinda un salto de linea
+            Console.Write("INGRESE SU EDAD: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if((edad>=1)&&(edad<=10))
+            {
+                Console.WriteLine();
+                Console.WriteLine("ES UN NIÑO");
+            }
+            if ((edad >= 11) && (edad <= 17))
+            {
+                Console.WriteLine();
+                Console.WriteLine("ES UN ADELOSCENTE");
+            }
+            if ((edad >= 18) && (edad <= 40))
+            {
+                Console.WriteLine();
+                Console.WriteLine("ES UN ADULTO");
+            }
+            if (edad >= 41)
+            {
+                Console.WriteLine();
+                Console.WriteLine("ES MAYOR");
+            }
+            Console.ReadKey();
+         }
+    }
+}
